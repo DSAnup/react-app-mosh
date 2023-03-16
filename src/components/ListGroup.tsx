dd93a2f590526b82d3747b0bfa906c4d12e2db7d
dsanup <anup12.m@gmail.com>
2023-03-16 21:11:42 +0600

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,12 @@
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Dhaka", "Tokyo", "London"];
+  let items = ["New York", "San Francisco", "Dhaka", "Tokyo", "London"];
+
+  items = [];
+
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li className="list-group-item">{item}</li>
