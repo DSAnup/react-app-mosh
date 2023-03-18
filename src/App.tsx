d417d8811794797310db3302c3ce12e514692d0b
dsanup <anup12.m@gmail.com>
2023-03-18 07:48:05 +0600

    building a button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import ListGroup from "./components/ListGroup";
-import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
-  let items = ["New York", "San Francisco", "Dhaka", "Tokyo", "London"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
-      <Alert>
-        Hello <span>World!</span>
-      </Alert>
+      <Button color="primary" onClick={() => console.log("Clicked")}>
+        Submit
+      </Button>
     </div>
   );
 }
